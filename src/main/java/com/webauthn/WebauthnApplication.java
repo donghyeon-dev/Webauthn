364f7f0a6bab25ff78deccc0f5816bb7666d990c
박동현 <donghyeondev@gmail.com>
2021-08-26 17:59:04 +0900

    미사용 Feign 의존성 제거

--- src/main/java/com/webauthn/WebauthnApplication.java
+++ src/main/java/com/webauthn/WebauthnApplication.java
@@ -2,10 +2,8 @@ package com.webauthn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class WebauthnApplication {
 
     public static void main(String[] args) {
